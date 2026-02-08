5eae1ac15cce12c881ee572866754c3b8d21e2c3
bytem0use <webofmusic3@gmail.com>
2026-02-08 10:02:33 -0800

    Figuring out

--- src/main/java/net/bytem0use/core_power/api/abilities/keys/Ability4.java
+++ src/main/java/net/bytem0use/core_power/api/abilities/keys/Ability4.java
@@ -1,4 +1,14 @@
 package net.bytem0use.core_power.api.abilities.keys;
 
-public class Ability4 {
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+
+public class Ability4 extends KeyBinding {
+    public Ability4(String translationKey, int code, String category) {
+        super(translationKey, code, category);
+    }
+
+    public Ability4(String translationKey, InputUtil.Type type, int code, String category) {
+        super(translationKey, type, code, category);
+    }
 }
--- src/main/java/net/bytem0use/core_power/api/abilities/keys/Ability5.java
+++ src/main/java/net/bytem0use/core_power/api/abilities/keys/Ability5.java
@@ -1,4 +1,14 @@
 package net.bytem0use.core_power.api.abilities.keys;
 
-public class Ability5 {
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+
+public class Ability5 extends KeyBinding {
+    public Ability5(String translationKey, int code, String category) {
+        super(translationKey, code, category);
+    }
+
+    public Ability5(String translationKey, InputUtil.Type type, int code, String category) {
+        super(translationKey, type, code, category);
+    }
 }
